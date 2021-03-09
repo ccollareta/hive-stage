536c89ed67a75fe58820b311b1a21f0af60ce100
Christopher Collareta <christopher.collareta@loungelizard.com>
2021-03-09 13:17:38 -0500

    Fixes for hexagons

--- src/components/hero.js
+++ src/components/hero.js
@@ -26,7 +26,38 @@ const Hero = () => (
     <div className="container">
         <img data-depth="0.05" src={hex1} className="hex-img img-1" />
         <img data-depth="0.05" src={hex2} className="hex-img img-2" />
-        <img data-depth="0.05" src={hex3} className="hex-img img-3" />
+        <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg"
+                    xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 189.2 210.4"
+                    data-depth="0.05" style="enable-background:new 0 0 189.2 210.4;" xml:space="preserve"
+                    class="hex-img img-3">
+                    <defs>
+                        <linearGradient id="grad1" x1="0%" y1="100%" x2="100%" y2="0%">
+                            <stop offset="0%" style="stop-color:rgb(114,152,214);stop-opacity:1" />
+                            <stop offset="100%" style="stop-color:rgb(45,101,195);stop-opacity:1" />
+                        </linearGradient>
+                    </defs>
+                    <style type="text/css">
+                        .st0 {
+                            filter: url(#a);
+                        }
+
+                        .st1 {
+                            fill: url(#grad1)
+                        }
+                    </style>
+                    <filter filterUnits="userSpaceOnUse" height="286.77" id="a" width="294.966" x="0" y="0">
+                        <feOffset dy="3" input="SourceAlpha"></feOffset>
+                        <feGaussianBlur result="b" stdDeviation="3"></feGaussianBlur>
+                        <feFlood flood-opacity="0.161"></feFlood>
+                        <feComposite in2="b" operator="in"></feComposite>
+                        <feComposite in="SourceGraphic" operator="over"></feComposite>
+                    </filter>
+                    <g transform="matrix(1, 0, 0, 1, 0, 0)" class="st0">
+                        <path class="st1" d="M176.9,43.8c7.5,4.4,12.2,12.4,12.2,21.1l0.1,80.7c0,8.8-4.7,16.8-12.2,21.2l-70,40.3
+						c-7.6,4.3-16.8,4.3-24.4,0l-70.3-40.6c-7.5-4.4-12.2-12.4-12.2-21.1L0,64.7C0,56,4.7,47.9,12.2,43.5l70-40.3
+						c7.6-4.3,16.8-4.3,24.4,0L176.9,43.8z" />
+                    </g>
+                </svg>
         <div className="hero-action">
             <div className="row">
                 <div className="col-6">

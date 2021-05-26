406c63a65fc1cc095f25351bfc73c197b72b1b6a
Christopher Collareta <christopher.collareta@loungelizard.com>
2021-05-26 14:51:41 -0400

    Community Page

--- src/pages/content/overview.md
+++ src/pages/content/overview.md
@@ -1,16 +1,17 @@
 ---
 top_section:
-  title: Our Community
+  title: Our Community                                 Handbook
   hero_image: /images/hero-bg2.png
-  text_block: "The HIVE Community continues to make connections between and within
-    government and industry. Join us as we revolutionize Federal Health IT
-    through collaborative vision and innovation. "
+  text_block: >-
+    Enrich your network, connect with government stakeholders and industry
+    partners, and
+                                    become a part of the solution.
   button:
     button_text: Join the Conversation
     button_link: https://hivegov.mobilize.io/main/groups/43575/lounge
 blocks:
   - block:
-      icon: /images/slide-img.svg
+      icon: /images/info5.svg
       text: We continue to find partners throughout industry and government to help
         fill the needs of PEO DHMS and its partners. It all starts with
         understanding what people, process, transformations, and technologies
@@ -35,4 +36,48 @@ blocks:
       url: https://hivegov.mobilize.io/main/groups/43511/lounge
       title: Connect With Us
       subtitle: START BUZZING
+three_blocks:
+  small_text: BEE A PART OF THE INNOVATION
+  headline: Transparency At Scale
+  description: The goal of our community is to streamline Industry to Government,
+    Government to Government, and Industry to Industry interactions to
+    accelerate the adoption of transformative ideas across Federal Health
+    agencies. Follow these three steps to be a part of the buzz!
+  block_one:
+    icon: /images/info5.svg
+    title: Enter The HIVE
+    description: Register and join the HIVE Community! Once you are signed up, join
+      different groups to explore opportunities and be up to date on news and
+      events.
+    link: "#"
+  block_two:
+    icon: /images/info6.svg
+    title: Get Involved
+    description: Post on the HIVE Community and provide information about yourself
+      and your organization.
+    link: "#"
+  block_three:
+    icon: /images/info7.svg
+    title: Innovate Together
+    description: Help the Federal Health community drive towards the future by
+      partnering on both industry and government opportunities.
+    link: "#"
+com_blocks:
+  small_text: "@2"
+  headline: "@"
+  description: "@"
+  block_one:
+    icon: /images/hive-comm.png
+    description: Help the Federal Health community drive towards the future by
+      partnering on both industry and government opportunities.
+    title: HIVE Community
+    link: https://community.hive.gov/main/groups/43511/lounge
+  block_two:
+    link: https://community.hive.gov/main/groups/43576/lounge
+    icon: /images/teaming-landscape.png
+    description: The Teaming and Partnering community invites industry members to
+      introduce themselves, create teaming opportunities, and announce
+      partnerships. Join the conversation to introduce yourself and connect with
+      other organizations.
+    title: Teaming & Partnering
 ---
--- src/pages/our-community.js
+++ src/pages/our-community.js
@@ -11,24 +11,18 @@ import { Link, withPrefix, graphql } from 'gatsby';
 import hero_bg from '../images/hero-bg-3.jpg';
 import { Helmet } from 'react-helmet';
 import link_arrow from '../images/link-arrow.png';
-import conv from '../images/conv.png';
-import art_1 from '../images/section-art-1.png';
-import art_2 from '../images/section-art-2.png'
-import c1 from '../images/c1.png';
-import c2 from '../images/c2.png';
-import c3 from '../images/c3.png';
-import idea_1 from '../images/idea1.png';
-import idea_2 from '../images/idea2.png';
-import idea_3 from '../images/idea3.png';
-import hex_2 from '../images/Join-The-Conversation-lowpoly_v2.png'
-import hex_3 from '../images/hero-hex-3.svg'
-import art1 from '../images/art1.png'
-import art5 from '../images/art5.png'
+import hover_icon from '../images/new/hover-icon.svg';
+import art1 from '../images/new/art1.png'
+import art5 from '../images/new/art5.png'
+import art6 from '../images/new/art6.png'
 import colbg from '../images/col-bg.jpg';
-import hex1 from '../images/hex-1.svg'
-import hex2 from '../images/hex-2.svg'
-import hex3 from '../images/hex-3.svg'
-import art2 from '../images/art2.png'
+import hex5 from '../images/new/hex-5.svg'
+import hex6 from '../images/new/hex-6.svg'
+import hex4 from '../images/new/hex-4.svg'
+import hex1 from '../images/new/hex-1.svg'
+import hex2 from '../images/new/hex-2.svg'
+import hex3 from '../images/new/hex-3.svg'
+import hero_hex from '../images/new/hero-hex3.png'
 import art3 from '../images/art3.png'
 
 
@@ -52,20 +46,21 @@ const OverviewPage = ({data}) => {
     <SEO title="Technology Trends in Healthcare-Join Our Community"
     keywords="technology trends in healthcare (MSV 4,400)"
     description="The HIVE Community is a vibrant and resourceful place to make connections between and within the technology industry and Federal Health IT. Join us today." />
-<section className="hero-section-2 style-2 animated-hex"
-            style={{
+        <section className="hero-section-2 style-2 animated-hex" style={{
                 backgroundImage: `url(${posts.top_section.hero_image})`
               }}>
             <img src={art1} className="art" />
             <div className="hero-hex">
-                
-                <img src={hex_2} /> 
+                <img src={hero_hex} />
             </div>
             <div className="container">
+                <img data-depth="0.05" src={hex5} className="hex-img img-1" />
+                <img data-depth="0.05" src={hex6} className="hex-img img-2" />
+                <img data-depth="0.05" src={hex4} className="hex-img img-3" />
                 <div className="hero-action">
                     <div className="row">
-                        <div className="col-6">
-                            <h1 className="hero-title text-white"><span>{posts.top_section.title}</span></h1>
+                        <div className="col-7">
+                            <h1 className="hero-title text-white">{posts.top_section.title}</h1>
                             <p className="hero-subtitle text-white">
                             {posts.top_section.text_block}
                             </p>
@@ -77,70 +72,128 @@ const OverviewPage = ({data}) => {
                 </div>
             </div>
         </section>
-        <section className="hive-slider-section">
-            <img src={art5} className="art" />
-            <div className="inner-container">
-                <div className="slider-container">
-                    <div className="feature-slider">
-                    {posts.blocks.slice(0, 3).map((block,index) => (
-                        <div className="feature-item" key={index}>
-                            <div className="row">
-                                <div className="col-6 text">
-                                    <div className="section-head">
-                                        <div className="section-title">
-                                            <p className="sub-title text-orange-alt">{block.block.subtitle}</p>
-                                            <h2 className="title text-white"><a href={block.block.url} className="text-dark">{block.block.title}</a></h2>
-                                        </div>
-                                    </div>
-                                    <p>
-                                        {block.block.text}
-                                    </p>
-                                    <a href={block.block.url} className="btn rounded"><span>Read More</span></a>
-                                </div>
-                                <div className="col-6 image">
-                                    <img src={block.block.icon} className="img-fluid" alt="img" />
-                                </div>
+        
+        <section className="hive-cards-section style-3">
+            <img src={art6} className="art art-1" />
+            <img src={art5} className="art art-2" />
+            <div className="container">
+                <div className="row">
+                    <div className="col col-7">
+                        <div className="section-head">
+                            <div className="section-title">
+                                <p className="sub-title text-blue">{posts.three_blocks.small_text}</p>
+                                <h2 className="title">{posts.three_blocks.headline}</h2>
                             </div>
                         </div>
-                         ))}
+                        <p className="section-subhead">
+                        {posts.three_blocks.description}
+                        </p>
+                    </div>
+                </div>
+                <div className="row cards-row">
+                    <div className="col col-4">
+                        <div className="card">
+                            <img src={posts.three_blocks.block_one.icon} className="card-icon" />
+                            <h4 className="card-title">{posts.three_blocks.block_one.title}</h4>
+                            <p className="card-body">
+                            {posts.three_blocks.block_one.description}
+                            </p>
+                        </div>
+                    </div>
+                    <div className="col col-4">
+                        <div className="card">
+                            <img src={posts.three_blocks.block_two.icon} className="card-icon" />
+                            <h4 className="card-title">{posts.three_blocks.block_two.title}</h4>
+                            <p className="card-body">
+                            {posts.three_blocks.block_two.description}
+                            </p>
+                        </div>
+                    </div>
+                    <div className="col col-4">
+                        <div className="card">
+                            <img src={posts.three_blocks.block_three.icon} className="card-icon" />
+                            <h4 className="card-title">{posts.three_blocks.block_three.title}</h4>
+                            <p className="card-body">
+                            {posts.three_blocks.block_three.description}
+                            </p>
+                        </div>
                     </div>
                 </div>
             </div>
         </section>
-        <section className="hive-3-col-section-2 animated-hex" style={{
+        <section className="hive-2-col-section animated-hex" style={{
           backgroundImage: `url(${colbg})`
           }}>
-            
+            <img data-depth="0.05" src={hex1} className="hex-img img-1" />
+            <img data-depth="0.05" src={hex2} className="hex-img img-2" />
+            <img data-depth="0.05" src={hex3} className="hex-img img-3" />
             <div className="inner-container">
                 <div className="section-head">
                     <div className="section-title">
                         <p className="sub-title text-orange">The pollination platform</p>
-                        <h2 className="title text-white">HIVE Opportunity Buzz</h2>
+                        <h2 className="title text-white">Join Our Network</h2>
                     </div>
-                    <div className="link d-link">
-                        <a href="/opportunities" className="font-exo text-white">See all opportunities <img
+                    <div className="link d-link" style={{
+                      '--width' : '190.906px',
+                    }}>
+                        <a href="https://community.hive.gov" className="font-exo text-white">ENTER THE HIVE <img
                                 src={link_arrow} /></a>
                     </div>
                 </div>
-                <div className="row ">
-                {ideas.map(feat => (
-      <div key={feat.title} className="col col-4">
-        <FeatOpp title={feat.title}
-        description = {feat.excerpt}
-        author = {feat.author}
-        date = {feat.date}
-        thumbnail = {feat.thumbnail}
-        link = {feat.path}/>
-      </div>
-    )) }
-                    
+                <div className="row">
+                    <div className="col col-6">
+                        <div className="card">
+                            <div className="card-img">
+                                <img src={posts.com_blocks.block_one.icon} className="img-fluid" />
+                                <a href="#" className="card-overlay">
+                                    <img src={hover_icon} />
+                                </a>
+                            </div>
+                            <div className="card-body">
+                                <a href="javascript:void(0)">
+                                    <h4 className="text-white">{posts.com_blocks.block_one.title}</h4>
+                                </a>
+                                <p className="text-white">
+                                {posts.com_blocks.block_one.description}
+                                </p>
+                                <div className="link">
+                                    <a href={posts.com_blocks.block_one.link} className="font-exo text-white">
+                                        Learn More <img src={link_arrow} />
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col col-6">
+                        <div className="card">
+                            <div className="card-img">
+                                <img src={posts.com_blocks.block_two.icon} className="img-fluid" />
+                                <a href="#" className="card-overlay">
+                                    <img src={hover_icon} />
+                                </a>
+                            </div>
+                            <div className="card-body">
+                                <a href="javascript:void(0)">
+                                    <h4 className="text-white">{posts.com_blocks.block_two.title}</h4>
+                                </a>
+                                <p className="text-white">
+                                {posts.com_blocks.block_two.description}
+                                </p>
+                                <div className="link">
+                                    <a href={posts.com_blocks.block_two.link} className="font-exo text-white">
+                                        Learn More <img src={link_arrow} />
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 <div className="link m-link">
-                    <a href="/opportunities" className="font-exo text-white">See all opportunities <img
-                            src={link_arrow} /></a>
+                    <a href="https://community.hive.gov" className="font-exo text-white">ENTER THE HIVE <img src={link_arrow} /></a>
                 </div>
             </div>
         </section>
+        
     
     
   </Layout>
@@ -160,14 +213,43 @@ const OverviewPage = ({data}) => {
                 button_link
             }
         }
-        blocks{
-            block{
-                icon
-                text
-                subtitle
-                title
-                url
-            }
+        three_blocks {
+          description
+          headline
+          small_text
+          block_one {
+            description
+            icon
+      
+            title
+          }
+          block_three {
+            description
+            icon
+            
+            title
+          }
+          block_two {
+            description
+            icon
+            title
+            
+          }
+        }
+        com_blocks {
+          
+          block_one {
+            description
+            icon
+            link
+            title
+          }
+          block_two {
+            description
+            icon
+            title
+            link
+          }
         }
         }
       }
